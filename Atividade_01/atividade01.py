f5d83143de6580db45aa0dd21e4b9e96f5b51a80
Renan Saldanha <renansaldlinhares@gmail.com>
2021-06-13 16:40:34 -0300

    Add exceptions

--- Atividade_01/atividade01.py
+++ Atividade_01/atividade01.py
@@ -3,75 +3,130 @@
 __Author__ = "Renan Saldanha"
 
 import json
-from typing import Dict
 
 
 def createFileJson(dictionary=None):
-    if dictionary is None:
-        print("O dicionário é vazio")
-        return
-    with open("Drinks.json", "w") as file:
-        json.dump(dictionary, file)
-    print("Arquivo .json criado com sucesso\n\n")
+    """
+    This function create a json file with the dictionary recived as param
+
+    :param dictionary: Reciver a dictionary;
+    :type dictionary: Dictionary 
+    :return : Do a screen print message os success or fail case dictionary is None;
+    :rtype : Void
+    """
+
+    try:
+        if dictionary is None:
+            print("O dicionário é vazio")
+            return
+        with open("Drinks.json", "w") as file:
+            json.dump(dictionary, file)
+        print("Arquivo .json criado com sucesso\n\n")
+    except Exception as e:
+        print(f'Uma exceção ocorreu\n {e}')
 
 
 def printDrinks(dictionary=None):
-    if dictionary is None:
-        print("Dicionário inválido\n")
-        return
-    for key in dictionary:
-        print("\n\n")
-        print(key)
-        print(f'\tNome: {dictionary[key]["nome"]}')
-        print(f'\tPreço: {dictionary[key]["preco"]}')
-        print(f'\tRestrição: {dictionary[key]["restricao"]}')
-        print(f'\tTeor Alcoolico: {dictionary[key]["teorAlcoolico"]}')
-        print(f'\tQuantidade em Estoque: {dictionary[key]["quantidade"]}')
+    """
+    This function print a formatted table of drinks
+
+    :param dictionary: Reciver a dictionary;
+    :type dictionary: Dictionary 
+    :return : Do a screen print message with params of dictionary or fail case dictionary is None;
+    :rtype : Void
+    """
+
+    try:
+        if dictionary is None:
+            print("Dicionário inválido\n")
+            return
+        for key in dictionary:
+            print("\n\n")
+            print(key)
+            print(f'\tNome: {dictionary[key]["nome"]}')
+            print(f'\tPreço: {dictionary[key]["preco"]}')
+            print(f'\tRestrição: {dictionary[key]["restricao"]}')
+            print(f'\tTeor Alcoolico: {dictionary[key]["teorAlcoolico"]}')
+            print(f'\tQuantidade em Estoque: {dictionary[key]["quantidade"]}')
+    except Exception as e:
+        print(f'Uma exceção ocorreu\n {e}')
 
 
 def readFileJson():
-    dictionary = {}
-    with open("Drinks.json", "r") as file:
-        dictionary = json.load(file)
-    print(8*"*" + "Minhas Bebidas" + 8*"*")
-    printDrinks(dictionary)
-    print("\n\n\n\n\n\n" + 8*("*")+"Arquivo JSON"+8*('*')+"\n\n")
-    print(dictionary)
+    """
+    This function read a json file and sava datas in a dictionary
+
+    :return : Do a screen print of file's content;
+    :rtype : Void
+    """
+
+    try:
+        dictionary = {}
+        with open("Drinks.json", "r") as file:
+            dictionary = json.load(file)
+        print(8*"*" + "Minhas Bebidas" + 8*"*")
+        printDrinks(dictionary)
+        print("\n\n\n\n\n\n" + 8*("*")+"Arquivo JSON"+8*('*')+"\n\n")
+        print(dictionary)
+    except Exception as e:
+        print(f'Uma exceção ocorreu\n {e}')
 
 
 def CreateFileTsv(dictionary=None):
-    if dictionary is None:
-        print("O dicionário é vazio")
-        return
-    with open("Drinks.tsv", "w") as file:
-        file.write("id\tnome\tpreco\trestricao\tteorAlcoolico\tquantidade\n")
-        for key in dictionary:
-            nome = dictionary[key]["nome"]
-            preco = dictionary[key]["preco"]
-            restricao = dictionary[key]["restricao"]
-            teorAlcoolico = dictionary[key]["teorAlcoolico"]
-            quantidade = dictionary[key]["quantidade"]
+    """
+    This function create a .tsv file with the dictionary recived as param
+
+    :param dictionary: Reciver a dictionary;
+    :type dictionary: Dictionary 
+    :return : Do a screen print message os success or fail case dictionary is None;
+    :rtype : Void
+    """
+
+    try:
+        if dictionary is None:
+            print("O dicionário é vazio")
+            return
+        with open("Drinks.tsv", "w") as file:
             file.write(
-                f'{key}\t{nome}\t{preco}\t{restricao}\t{teorAlcoolico}\t{quantidade}\n')
+                "id\tnome\tpreco\trestricao\tteorAlcoolico\tquantidade\n")
+            for key in dictionary:
+                nome = dictionary[key]["nome"]
+                preco = dictionary[key]["preco"]
+                restricao = dictionary[key]["restricao"]
+                teorAlcoolico = dictionary[key]["teorAlcoolico"]
+                quantidade = dictionary[key]["quantidade"]
+                file.write(
+                    f'{key}\t{nome}\t{preco}\t{restricao}\t{teorAlcoolico}\t{quantidade}\n')
         print("Arquivo .tsv criado com sucesso\n\n")
+    except Exception as e:
+        print(f'Uma exceção ocorreu\n {e}')
 
 
 def readFileTsv():
-    dictionary = {}
-    with open("Drinks.tsv", "r") as file:
-        str = file.readline()
-        keys = str.strip().split("\t")
-        str = file.readline()
-        while(str):
-            values = str.strip().split("\t")
-            dictionary[values[0]] = {keys[1]: values[1], keys[2]: values[2],
-                                     keys[3]: values[3], keys[4]: values[4], keys[5]: values[5]}
+    """
+    This function read a .tsv file and sava datas in a dictionary
+
+    :return : Do a screen print of file's content;
+    :rtype : Void
+    """
+    try:
+        dictionary = {}
+        with open("Drinks.tsv", "r") as file:
             str = file.readline()
-    print(8*"*" + "Minhas Bebidas" + 8*"*")
-    printDrinks(dictionary)
-    print("\n\n\n\n\n\n" + 8*("*") +
-          "Variável dictionary carregada do arquivo .tsv"+8*('*')+"\n\n")
-    print(dictionary)
+            keys = str.strip().split("\t")
+            str = file.readline()
+            while(str):
+                values = str.strip().split("\t")
+                dictionary[values[0]] = {keys[1]: values[1], keys[2]: values[2],
+                                         keys[3]: values[3], keys[4]: values[4], keys[5]: values[5]}
+                str = file.readline()
+        print(8*"*" + "Minhas Bebidas" + 8*"*")
+        printDrinks(dictionary)
+        print("\n\n\n\n\n\n" + 8*("*") +
+              "Variável dictionary carregada do arquivo .tsv"+8*('*')+"\n\n")
+        print(dictionary)
+    except Exception as e:
+        print(f'Uma exceção ocorreu\n {e}')
 
 
 def main():
